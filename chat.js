973177deb0d0322e43baa0127841f043c97b460b
INGCapaDev <ingcapadev@gmail.com>
2024-03-03 23:18:00 -0700

    :recycle: [REFACTOR](AI): Refactor the chat node file

--- chat.js
+++ chat.js
@@ -2,6 +2,14 @@ import { createBrotliCompress } from 'node:zlib';
 import { openAI } from './openai.js';
 import readline from 'node:readline';
 
+const INITIAL_HISTORY = [
+  {
+    role: 'system',
+    content:
+      'You are an AI assistant, answer any questions to the best of your ability. !!!LETS THINK STEP BY STEP!!!',
+  },
+];
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -22,24 +30,28 @@ const newMessage = async (history, message) => {
 
 const formatMessage = (userInput) => ({ role: 'user', content: userInput });
 
+const userWantToQuit = (userInput) => {
+  const quitWords = ['quit', 'exit', 'goodbye', 'bye'];
+  if (quitWords.includes(userInput.toLowerCase())) {
+    console.log('\n\nAI: Goodbye!, see you soon.');
+  }
+  return quitWords.includes(userInput.toLowerCase());
+};
+
+const think = async (userInput, history) => {
+  console.log('Thinking...');
+  const userMessage = formatMessage(userInput);
+  const response = await newMessage(history, userMessage);
+  history.push(userMessage, response);
+  console.log(`\n\nAI: ${response.content}`);
+};
+
 const chat = async () => {
-  let history = [
-    {
-      role: 'system',
-      content:
-        'You are an AI assistant, answer any questions to the best of your ability. !!!LETS THINK STEP BY STEP!!!',
-    },
-  ];
+  let history = INITIAL_HISTORY;
   const start = () => {
-    rl.question('You: ', async (userInput) => {
-      if (userInput.toLowerCase() === 'exit') {
-        rl.close();
-        return;
-      }
-      const userMessage = formatMessage(userInput);
-      const response = await newMessage(history, userMessage);
-      history.push(userMessage, response);
-      console.log(`\n\nAI: ${response.content}\n\n`);
+    rl.question('\n\nYou: ', async (userInput) => {
+      if (userWantToQuit(userInput)) return rl.close();
+      await think(userInput, history);
       start();
     });
   };
